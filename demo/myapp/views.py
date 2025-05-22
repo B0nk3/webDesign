f07d71ee1721b7610b5c433fa82e1c2b625c1f00
Alexandru Carnaru <alexandru.carnaru05@e-uvt.ro>
2025-05-22 12:33:00 +0300

    Delete demo directory

--- demo/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-# Create your views here.
-def home(request):
-    return render(request, "home.html")
-from .models import TodoItem
-def todos(request):
-    items = todoitem.object.all()
-    return render (request, "todos.html", {"todos: items"})
